76c9e45ecc0610dca95b379be8f8e98540a55388
leikooo <108205879+lieeew@users.noreply.github.com>
2023-11-26 11:26:37 +0800

    init

---
+++ config/routes.ts
@@ -0,0 +1,19 @@
+﻿export default [
+  { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }, { path: '/user/register', component: './User/Register'}] },
+  { path: '/', redirect: '/add_chart' },
+  { path: '/add_chart', name: '智能分析', icon: 'barChart', component: './AddChart' },
+  { path: '/add_chart_async', name: '智能分析（异步）', icon: 'barChart', component: './AddChartAsync' },
+  { path: '/add_chart_mq', name: '智能分析（消息队列）', icon: 'barChart', component: './AddChartMQ' },
+  { path: '/my_chart', name: '我的图表', icon: 'pieChart', component: './MyChart' },
+  {
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', name: '管理页面', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '管理页面2', component: './Admin' },
+    ],
+  },
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
---
+++ src/pages/User/Register/index.tsx
@@ -0,0 +1,151 @@
+import Footer from '@/components/Footer';
+import { listChartByPageUsingPOST } from '@/services/yubi/chartController';
+import { userRegisterUsingPOST } from '@/services/yubi/userController';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginForm, ProFormText } from '@ant-design/pro-components';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { Helmet, history } from '@umijs/max';
+import { message, Tabs } from 'antd';
+import React, { useEffect, useState } from 'react';
+import Settings from '../../../../config/defaultSettings';
+import {Link} from "umi";
+
+const Register: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    };
+  });
+
+  useEffect(() => {
+    listChartByPageUsingPOST({}).then((res) => {
+      console.error('res', res);
+    });
+  });
+
+  const handleSubmit = async (values: API.UserRegisterRequest) => {
+    try {
+      // 注册
+      const res = await userRegisterUsingPOST(values);
+      if (res.code === 0) {
+        const defaultLoginSuccessMessage = '注册成功！';
+        message.success(defaultLoginSuccessMessage);
+        const urlParams = new URL(window.location.href).searchParams;
+        history.push(urlParams.get('redirect') || '/');
+        return;
+      } else {
+        message.error(res.message);
+      }
+    } catch (error) {
+      const defaultLoginFailureMessage = '注册失败，请重试！';
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  return (
+    <div className={containerClassName}>
+      <Helmet>
+        <title>
+          {'注册'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" src="/logo.svg" />}
+          title="鱼智能 BI"
+          subTitle={
+            <a href="https://yupi.icu" target="_blank" rel="noreferrer">
+              编程导航知识星球的原创项目
+            </a>
+          }
+          onFinish={async (values) => {
+            await handleSubmit(values as API.UserLoginRequest);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '注册账号',
+              },
+            ]}
+          />
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={'请输入用户名'}
+                rules={[
+                  {
+                    required: true,
+                    message: '用户名是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请再次输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                ]}
+              />
+            </>
+          )}
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+            <Link to="/user/login">登录</Link>
+          </div>
+        </LoginForm>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Register;
