332aa8357b27fda5dbce0e72817b561565622353
leikooo <108205879+lieeew@users.noreply.github.com>
2024-09-07 22:01:36 +0800

    update

--- src/app.tsx
+++ src/app.tsx
@@ -121,7 +121,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request = {
-  baseURL: 'https://bi-backend.leikooo.cn',
+  baseURL: 'http://1.117.228.86:8080',
   withCredentials: true,
   ...errorConfig,
 };
